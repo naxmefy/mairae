acdee7acb8688dd4947fe0563aa3c3d7194a51d7
MRW Neundorf <matt@nax.me>
2014-09-30 19:10:58 +0200

    added error handling

--- manager/routes/admin/index.js
+++ manager/routes/admin/index.js
@@ -1,7 +1,36 @@
 // app = manager
 module.exports = function (app) {
   var common = app.get('common');
-  app.use(common.Middlewares.auth.isAdmin);
+  app.use('/admin', common.Middlewares.auth.isAdmin);
 
   app.use('/admin', require('./dashboard'));
+  // catch 404 and forward to error handler
+  app.use('/admin*', function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  });
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+      app.use('/admin*', function(err, req, res, next) {
+          res.status(err.status || 500);
+          res.render('modules/admin/error', {
+              message: err.message,
+              error: err
+          });
+      });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use('/admin*', function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('modules/admin/error', {
+          message: err.message
+      });
+  });
 };
